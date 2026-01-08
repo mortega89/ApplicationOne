daf2081f63f04993641188e806c253da38754724
mortega89 <landi.lorenzo@outlook.com>
2026-01-08 11:17:43 +0100

    feat: add redirection test echo

--- appMain/script.sh
+++ appMain/script.sh
@@ -3,4 +3,4 @@
 
 echo "$(date +%d-%m-%Y//%H:%m:%S): Hello mum!" >>  /var/log/ApplicationOne.log
 # test
-echo "$(date +%d-%m-%Y//%H:%m:%S): ${DB_HOST} ${DB_PORT}"
+echo "$(date +%d-%m-%Y//%H:%m:%S): ${DB_HOST} ${DB_PORT}" >>  /var/log/ApplicationOne.log
